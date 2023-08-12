0ca54fdb14cadd07aa565b97dd38f3eeb74dffce
paul-juang <pauljuang3@gmail.com>
2023-08-13 01:43:04 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-const https = require('https');
-
-const Stream = require("stream").Transform
-
-const fs = require('fs');
-
-const async = require("async");
-
 const express = require('express');
-
-const path = require('path');
-
 const app = express();
 
-const session = require('express-session');
-
-const bcrypt = require("bcryptjs") //bcrypt
-
-const cookieParser = require('cookie-parser');
-
-const MongodbSession = require('connect-mongodb-session')(session);
+const openai = require('openai');
 
 require('dotenv').config()
 
-const mongoose = require('mongoose');
+const path = require('path');
 
-mongoose.connect(process.env.MONGODB_URI,{ 
-  useUnifiedTopology:true, 
-  useNewUrlParser:true,
-  useCreateIndex:true 
-})
-.then(()=> console.log("Database connected ..."))
-.catch((err)=> console.log(err))   
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //app.set
 app.set("view engine", "ejs");
@@ -50,24 +26,6 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-const store = new MongodbSession({
-  uri: process.env.MONGODB_URI,
-  collection: "sessions"
-})
-
-/*app.use(session({
-  secret: "hashsecret",
-  resave: false,
-  saveUninitialized: false,
-  store: store
-}));*/
-
-//use router
-//app.use(require('./routes/session'));
-app.use(require('./routes/accounting'));
-app.use(require('./routes/agk'));
-app.use(require('./routes/loto'));
-app.use(require('./routes/misc'));
 
 app.listen(PORT, function() {
     console.log('Server listening on ' + PORT);
