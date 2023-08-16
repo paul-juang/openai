97b7233ce6bf1eedc963f409b1e8a3dd264d9309
paul-juang <pauljuang3@gmail.com>
2023-08-16 16:46:32 +0800

    update all files

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+  const { Configuration, OpenAIApi } = require("openai");
+
+  const configuration = new Configuration({
+    //apiKey: process.env.OPENAI_API_KEY,
+    apiKey: "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",
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const chatGPT = async (prompt) => {
+    try {
+         const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role:"user", content:prompt}]
+         })
+         console.log(response["data"]["choices"][0]["message"]["content"])
+       } 
+    catch(error) {
+        console.log({error})
+       }
+  }
+
+  let prompt = "a list of ten random cities in the United State";
+  chatGPT(prompt)
+  
+/*
 const express = require('express');
 const app = express();
 
-const openai = require('openai');
+const cors = require('cors');
 
-require('dotenv').config()
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+require('dotenv').config();
 
 const path = require('path');
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,12 +57,38 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(express.json());
 
+const bodyParser = require('body-parser');
+
+
+app.use(cors());
+
+
 
 //home page
 app.get("/", (req, res) => {
   res.render("chatgpt");
 });
 
-app.listen(PORT, function() {
+app.post("/temp", async (req, res) => {
+
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  async function start(argument) {
+    const response = await openai.createCompletion({
+      model: "text-devinci-003",
+      prompt: "list ten random cities in the United States",
+      tempreture: 0,
+      max_token: 1000
+    })
+    console.log(response.data.choices[0.text])
+  }
+  
+
+app.listen(PORT, () => {
     console.log('Server listening on ' + PORT);
 });
+*/
--- public/js/chatgpt.js
+++ public/js/chatgpt.js
@@ -1,4 +1,59 @@
 //chatGPT-OpenAi
 document.addEventListener('DOMContentLoaded', () => {
-       
-}
+	const formElement = document.querySelector("form");
+	const chatContainer = document.querySelector("#chat-container");
+	const prompt = document.querySelector("#prompt");
+
+	const button = document.querySelector("#btn")
+
+	button.addEventListener("click", async function(e) {
+		e.preventDefault()
+		const promptValue = prompt.value.trim()
+
+		let res = await fetch("/temp", {
+			      method: 'POST', 
+                  headers: {
+                     'Content-Type': 'application/json'
+                    },
+                  body: JSON.stringify({ prompt: promptValue })
+                  })
+
+        let txt = await res.json();
+        console.log(txt)
+		})
+
+
+ 
+	let loadInterval;
+
+	function loader(element) {
+		element.textContent = "";
+		loadInterval = setInterval(() => {
+			element.textContent += "."
+			if (element.textContent === "...") element.textContent = "";
+
+		}, 300)
+	}
+
+	function typeText(element, text) {
+		let index = 0;
+
+		let interval = setInterval(() => {
+			if (index < text.length) {
+			   element.innerHtml += text.charAt(index);
+			   index++;
+			} else {
+			   clearInterval(interval);
+			}
+		}, 200)
+	}
+
+	function generateUniqueId() {
+		const timestamp = Date.now();
+		const randomNumber = Math.random();
+		const hexString = randomNumber.toString(16);
+		return `id-${timestamp}-${hexString}`;
+	}
+  
+})
+
